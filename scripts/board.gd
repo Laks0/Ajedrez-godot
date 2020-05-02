c7ee5e9e488b73dc778c753b082b12b25fd6a213
Laks0 <laksjoaquin@gmail.com>
2020-05-02 00:21:25 -0300

    Posicion inicial

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,6 +1,11 @@
 extends TileMap
 
-export (PackedScene) var Piece
+export (PackedScene) var Peon
+export (PackedScene) var Caballo
+export (PackedScene) var Alfil
+export (PackedScene) var Dama
+export (PackedScene) var Rey
+export (PackedScene) var Torre
 
 var grid = []
 
@@ -13,15 +18,7 @@ func _ready():
 			t.append(null)
 		grid.append(t)
 	
-	var p = Piece.instance()
-	p.set_up(-1, Vector2(2,7))
-	add_child(p)
-	grid[2][7] = p
-	
-	var p2 = Piece.instance()
-	p2.set_up(1, Vector2(3,1))
-	add_child(p2)
-	grid[3][1] = p2
+	inicial()
 
 func _process(delta):
 	var click = Input.is_action_just_released("Click")
@@ -32,6 +29,12 @@ func _process(delta):
 		if p != null: # Si se clickeó una casilla con una pieza
 			if p.team == turn: # Si es su turno
 				p.new_sel() # Seleccionarla
+
+func spawn_piece(piece, pos, team):
+	var p = piece.instance()
+	p.set_up(team, pos)
+	grid[pos.x][pos.y] = p
+	add_child(p)
 
 func move_piece(from, to):
 	var pfrom = grid[from.x][from.y]
@@ -54,3 +57,27 @@ func at_grid(pos):
 
 func inside_grid(vector):
 	return vector.x >= 0 and vector.x < 8 and vector.y >= 0 and vector.y < 8 
+
+func inicial(): # Posicion inicial
+	# Peones
+	for x in range(0, 8):
+		spawn_piece(Peon, Vector2(x, 6), -1)
+		spawn_piece(Peon, Vector2(x, 1), 1)
+	# Torres
+	for x in range(0,2):
+		spawn_piece(Torre, Vector2(x * 7, 7), -1)
+		spawn_piece(Torre, Vector2(x * 7, 0), 1)
+	# Caballos
+	for x in range(0,2):
+		spawn_piece(Caballo, Vector2(1 + x * 5, 7), -1)
+		spawn_piece(Caballo, Vector2(1 + x * 5, 0), 1)
+	# Alfiles
+	for x in range(0,2):
+		spawn_piece(Alfil, Vector2(2 + x * 3, 7), -1)
+		spawn_piece(Alfil, Vector2(2 + x * 3, 0), 1)
+	# Damas
+	spawn_piece(Dama, Vector2(3, 7), -1)
+	spawn_piece(Dama, Vector2(4, 0), 1)
+	# Reyes
+	spawn_piece(Rey, Vector2(4, 7), -1)
+	spawn_piece(Rey, Vector2(3, 0), 1)
